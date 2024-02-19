e14470947d9940d6818a5faa9231e1ee2f9849c0
Vitória Ferreira <vitoria.m.oliveira@ufv.br>
2024-02-18 22:22:01 -0300

    tp3

---
+++ inf 112/tp3/README.md
@@ -0,0 +1,9 @@
+# TP3 INF112
+Trabalho Pratico 3 da disciplina INF112 da Universidade Federal de Viçosa. 
+
+```bash
+# Para compilar
+$ g++ Maze.cpp main.cpp
+# Para executar
+$ ./a.exe
+```
---
+++ inf 112/tp3/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "Maze.h"
+
+// para compilar: 
+// g++ Maze.cpp main.cpp
+
+int main()
+{
+    int n;
+
+    std::cout << "Insira o tamanho do labirinto: ";
+    std::cin >> n;
+
+    Maze maze(n);
+    maze.leMatriz();
+    maze.imprimeLabirinto();
+    maze.verificaSaidaLabirinto();
+
+    if(maze.getResposta()){
+        std::cout << "Ha uma saida do labirinto\n";
+    }
+    else {
+        std::cout << "Nao ha uma saida do labirinto\n";
+    }
+    return 0;
+}
