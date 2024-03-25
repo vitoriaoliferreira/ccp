1ef253957ef74c4c693ae3e9a4134c2aefccbbea
Vitória Ferreira <vitoria.m.oliveira@ufv.br>
2024-03-25 15:23:24 -0300

    Create buscaBinariaFuncoes1.cpp

---
+++ inf 213/pratica 3/buscaBinariaFuncoes1.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <iostream>
+#include <chrono>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <string>
+using namespace std;
+
+//Vamos usar variaveis globais nesse caso...
+//Isso nao é uma boa pratica, mas vai facilitar no seu entendimento/implementacao da busca binaria
+int taxaVideo,taxaDownload,tamanho;
+
+//Essa funcao devera retornar true sse for possível 
+//assistir a um vídeo de "tamanho" segundos, que gasta taxaVideo bytes por segundo para ser assistido
+//supondo que sua internet consegue baixar taxaDownload bytes por segundo.
+//tempoCarregamento indica quanto tempo voce irá aguardar para começar assistir ao vídeo
+//Implemente esta funcao
+bool consigoAssistirSemPausas(int tempoCarregamento) {
+	int bytesDisponiveis = taxaDownload * tempoCarregamento;
+	for (int i = 0; i<tamanho / taxaVideo; i++){
+		bytesDisponiveis -= taxaVideo;
+		if (bytesDisponiveis < 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+
+int main() {
+	cin >> taxaVideo >> taxaDownload >> tamanho;
+	for(int i=0;i<20;i++) {
+		bool result = consigoAssistirSemPausas(i);
+		cout << i << ": " << result << "\n";		
+	}
+}
